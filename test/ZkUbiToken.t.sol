f476c87bf8b7fe8b5ff5097ea70da949edb5bfa8
Nate Faber <nathaniel@njfaber.com>
2024-05-25 21:20:05 +0200

    rename test file

--- test/ZkUbiToken.t.sol
+++ test/ZkUbiToken.t.sol
@@ -2,16 +2,16 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {ZkToken} from "../src/ZkUbiToken.sol";
+import {ZkUbiToken} from "../src/ZkUbiToken.sol";
 import {SD59x18, sd} from "@prb/math/src/SD59x18.sol";
 import {UD60x18, ud} from "@prb/math/src/UD60x18.sol";
 
 contract CounterTest is Test {
-    ZkToken public token;
+    ZkUbiToken public token;
     address public alice = address(1);
 
     function setUp() public {
-        token = new ZkToken(5000e18, 0.05e11);
+        token = new ZkUbiToken(5000e18, 0.05e11);
         deal(alice, 1 ether);
         token.approveUser(alice);
     }
