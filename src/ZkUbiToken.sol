d37b2126f99a1d1645680fd4efdfba46e6dc55a2
Nate Faber <nathaniel@njfaber.com>
2024-05-25 21:10:49 +0200

    change algorithm to just calculate percentages that the value should move towards the target

--- src/ZkUbiToken.sol
+++ src/ZkUbiToken.sol
@@ -1,56 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
-import { SD59x18 } from "prb-math/src/SD59x18.sol";
-import { UD60x18 } from "@prb/math/src/UD60x18.sol";
+import {SD59x18, sd} from "@prb/math/src/SD59x18.sol";
+import {UD60x18, ud, convert as udconvert} from "@prb/math/src/UD60x18.sol";
+import {console} from "forge-std/Test.sol";
 
 contract ZkToken {
+    uint256 public targetBalance;
+    uint256 public percentCloserPerSecondE18;
+
     // Variables
-    uint256 public k; // Rate of addition per unit time (e.g., per day)
-    uint256 public decayConstant; // Half-life period in the same time units as rate of addition
+    UD60x18 public k; // Rate of addition per unit time (e.g., per day)
+    UD60x18 public decayConstant; // Half-life period in the same time units as rate of addition
 
     mapping(address user => uint256) balances;
     mapping(address user => uint256 timestamp) lastUpdate;
 
-    constructor(uint256 tokensPerSecond, uint256 percentLossPerYearE18) {
-        k = tokensPerSecond;
-        decayConstant = calculateDecayConstant(percentLossPerYearE18);
-    }
-
-    // Function to calculate the decay constant (lambda) given a percentage loss per year
-    function calculateDecayConstant(
-        uint256 percentLossPerYearE18
-    ) public pure returns (uint256) {
-        // Convert the percentage loss per year to a decimal
-        uint256 p = percentLossPerYearE18; 
-
-        // Calculate 1 - p
-        uint256 oneMinusP = (10 ** 18 - p);
-
-        // Calculate the natural logarithm of (1 - p) using a series approximation or a predefined library
-        int256 lnOneMinusP = ln(oneMinusP); // Assuming ln() returns value in 18 decimal format
-
-        // Since Solidity does not support floating point arithmetic, we'll keep precision using integers
-        uint256 lambda = uint256(-lnOneMinusP);
-
-        return lambda;
-    }
-
-    // Function to calculate natural logarithm using a series approximation (Taylor series or other method)
-    // This is a placeholder function, and a real implementation would be needed for accurate results
-    function ln(uint256 x) public pure returns (int256) {
-        // Placeholder implementation of natural logarithm
-        // Use a series approximation or a mathematical library for accurate results
-        // Example using a simple Taylor series expansion (not accurate for all ranges)
-        int256 result = 0;
-        uint256 num = 1;
-
-        while (num < 20) {
-            int256 sign = num % 2 == 0 ? int8(1) : int8(-1);
-            // Increase for better accuracy
-            result += sign * int256(((x - 1e18) ** num) / num );
-            num++;
-        }
-        return result;
+    constructor(uint256 _targetBalance, uint256 _percentCloserPerSecondE18) {
+        targetBalance = _targetBalance;
+        percentCloserPerSecondE18 = _percentCloserPerSecondE18;
     }
 
     function approveUser(address user) public {
@@ -61,27 +28,26 @@ contract ZkToken {
     // Function to calculate the total amount at time t
     function totalAmount(address account) public view returns (uint256) {
         uint256 N0 = balances[account];
-        uint256 lambda = decayConstant;
         uint256 timeElapsed = block.timestamp - lastUpdate[account];
+        bool goingUp = N0 < targetBalance;
+        uint256 distanceToGo = goingUp
+            ? targetBalance - N0
+            : N0 - targetBalance;
 
-        uint256 decayPart = N0 * expNeg(lambda * timeElapsed);
-        uint256 additionPart = (((k * 1e5) / lambda) *
-            (1e5 - expNeg(lambda * timeElapsed))) / 1e5;
+        UD60x18 percentToShrinkPerSecondE18 = ud(1e18) - ud(percentCloserPerSecondE18);
+        UD60x18 percentToShrinkPerDayE18 = percentToShrinkPerSecondE18.pow(udconvert(1 days));
 
-        return decayPart + additionPart;
-    }
+        UD60x18 nDays = udconvert(timeElapsed) / udconvert(1 days);
+        UD60x18 percentCloserNow =
+            percentToShrinkPerDayE18
+            .pow(nDays);
 
-    // Helper function to calculate e^(-x) using a series expansion approximation
-    function expNeg(uint256 x) internal pure returns (uint256) {
-        // Taylor series expansion for e^(-x) = 1 - x + x^2/2! - x^3/3! + ...
-        uint256 sum = 1e5; // 1 * 1e5 to maintain precision
-        uint256 term = 1e5; // First term is 1 * 1e5
+        UD60x18 newDistance = ud(distanceToGo).mul(percentCloserNow);
 
-        for (uint8 i = 1; i < 10; i++) {
-            term = (term * x) / (i * 1e5);
-            sum -= term;
-        }
-        return sum;
+        return
+            goingUp
+                ? targetBalance - newDistance.intoUint256()
+                : targetBalance + newDistance.intoUint256();
     }
 
     // Function to get the current total amount
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -3,32 +3,22 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import {ZkToken} from "../src/ZkUbiToken.sol";
+import {SD59x18, sd} from "@prb/math/src/SD59x18.sol";
+import {UD60x18, ud} from "@prb/math/src/UD60x18.sol";
 
 contract CounterTest is Test {
     ZkToken public token;
     address public alice = address(1);
 
     function setUp() public {
-        token = new ZkToken(1e18, 0.05e18);
+        token = new ZkToken(5000e18, 0.05e11);
         deal(alice, 1 ether);
         token.approveUser(alice);
     }
 
-    function test_ln_fn() public {
-        assertEq(token.ln(1e18), 0);
-        assertEq(token.ln(2e18), 693147180559945309);
-        assertEq(token.ln(3e18), 109861228866748869);
-    }
-
-    function test_decay_constant() public {
-        console.log('%e', token.decayConstant());
-        ZkToken token2 = new ZkToken(100, 0.5e18);
-        console.log('%e', token2.decayConstant());
-    }
-
-    function test_alice_balance_approaches() public {
-        vm.warp(100);
-        console.log(token.totalAmount(alice));
+    function test_alice_balance_approaches_target() public {
+        vm.warp(365 days);
+        console.log('%e', token.totalAmount(alice));
         // uint256 expectedAliceBalanceAtInfinity = 100 / token.decayConstant()
     }
 }
